a46bbc2c74b35c8657ac6be1ad7f59c1b500d37c
AleFurgala <ale.furgala@gmail.com>
2023-06-22 22:06:45 +0200

    added menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,121 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        System.out.println("Hi");
+
+        System.out.println("Program księgarnia. Wybierz menu: ");
+
+        System.out.println("1.Książki");
+
+        System.out.println("2.Klienci");
+        System.out.println("3.Zamówienia");
+
+        Scanner scanner = new Scanner(System.in);
+        int menu = scanner.nextInt();
+        switch (menu) {
+            case 1:
+                System.out.println("1.Wyświetl wszystkie książki");
+                System.out.println("2.Wyszukaj ksiażkę");
+                System.out.println("3.Dodaj książkę");
+                System.out.println("4.Usuń książkę");
+                System.out.println("5.Edytuj książkę");
+                System.out.println("6.Powrót do glównego menu");
+
+
+                int menu2 = scanner.nextInt();
+                switch (menu2) {
+                    case 1:
+                        System.out.println("1.Wyświetl wszystkie książki");
+                        break;
+                    case 2:
+                        System.out.println("2.Wyszukaj ksiażkę");
+                        break;
+                    case 3:
+                        System.out.println("3.Dodaj książkę");
+                        break;
+                    case 4:
+                        System.out.println("4.Usuń książkę");
+                        break;
+                    case 5:
+                        System.out.println("5.Edytuj książkę");
+                        break;
+                    case 6:
+                        System.out.println("6.Powrót do glównego menu");
+                        break;
+                    default:
+                        System.out.println("Wprowadziłeś błędny numer ");
+                }
+
+                break;
+            case 2:
+                System.out.println("1.Wyświetl wszystkich klienótw");
+                System.out.println("2.Wyszukaj klienta");
+                System.out.println("3.Dodaj klienta");
+                System.out.println("4.Usuń klienta");
+                System.out.println("5.Edytuj klienta");
+                System.out.println("6.Powrót do glównego menu");
+
+                int menu3 = scanner.nextInt();
+                switch (menu3) {
+                    case 1:
+                        System.out.println("1.Wyświetl wszystkie klienci");
+                        break;
+                    case 2:
+                        System.out.println("2.Wyszukaj klienci");
+                        break;
+                    case 3:
+                        System.out.println("3.Dodaj klienci");
+                        break;
+                    case 4:
+                        System.out.println("4.Usuń klienci");
+                        break;
+                    case 5:
+                        System.out.println("5.Edytuj klienci");
+                        break;
+                    case 6:
+                        System.out.println("6.Powrót do glównego menu");
+                        break;
+                    default:
+                        System.out.println("Wprowadziłeś błędny numer ");
+                        break;
+                }
+            case 3:
+                System.out.println("1.Wyświetl wszystkie zamówienia");
+                System.out.println("2.Wyszukaj zamówienie");
+                System.out.println("3.Dodaj zamówienie");
+                System.out.println("4.Usuń zamówienie");
+                System.out.println("5.Edytuj zamówienie");
+                System.out.println("6.Powrót do glównego menu");
+
+                int menu4 = scanner.nextInt();
+                switch (menu4) {
+                    case 1:
+                        System.out.println("1.Wyświetl wszystkie zamówienia");
+                        break;
+                    case 2:
+                        System.out.println("2.Wyszukaj zamówienia");
+                        break;
+                    case 3:
+                        System.out.println("3.Dodaj zamówienia");
+                        break;
+                    case 4:
+                        System.out.println("4.Usuń zamówienia");
+                        break;
+                    case 5:
+                        System.out.println("5.Edytuj zamówienia");
+                        break;
+                    case 6:
+                        System.out.println("6.Powrót do glównego menu");
+                        break;
+                    default:
+                        System.out.println("Wprowadziłeś błędny numer ");
+                }
+
+                break;
+            default:
+                System.out.println("Wprowadziłeś blędny numer");
+        }
+
     }
 }
