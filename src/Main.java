7f92a2bd6e8f6d5f8321cd12d4877f0cfdeb5db8
AleFurgala <ale.furgala@gmail.com>
2023-07-17 17:20:20 +0200

    Added login and password checking

--- src/Main.java
+++ src/Main.java
@@ -4,135 +4,149 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        JdbConnection jdbConnection = new JdbConnection();
-        Connection connection = jdbConnection.getConnection();
-        Book book = new Book(connection);
-        Client client = new Client(connection);
-        Order order = new Order(connection);
+        final String login = "Admin1";
+        final String password = "haslo1";
 
-        System.out.println("Program księgarnia. Wybierz menu: ");
-        int menu;
-        do {
-            System.out.println("1.Książki \n2.Klienci \n3.Zamówienia \n0.Zamknij program ");
+        String loginUser;
+        String passwordUser;
 
-            Scanner scanner = new Scanner(System.in);
-            menu = scanner.nextInt();
+        System.out.println("Podaj login i hasło");
+        Scanner scanner1 = new Scanner(System.in);
+        loginUser = scanner1.nextLine();
+        passwordUser = scanner1.nextLine();
+        if (login.equals(loginUser) && password.equals(passwordUser)) {
+            JdbConnection jdbConnection = new JdbConnection();
+            Connection connection = jdbConnection.getConnection();
+            Book book = new Book(connection);
+            Client client = new Client(connection);
+            Order order = new Order(connection);
 
-            switch (menu) {
-                case 1:
-                    System.out.println("1.Wyświetl wszystkie książki \n2.Wyszukaj ksiażkę \n3.Dodaj książkę \n4.Usuń książkę \n5.Edytuj książke \n6.Powrót do glównego menu \n********************************************************");
+            System.out.println("Program księgarnia. Wybierz menu: ");
+            int menu;
+            do {
+                System.out.println("1.Książki \n2.Klienci \n3.Zamówienia \n0.Zamknij program ");
 
-                    int menu2 = scanner.nextInt();
-                    switch (menu2) {
-                        case 1:
-                            book.showAllBooks();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 2:
-                            book.showBooksByTitleOrAuthor();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 3:
-                            book.addBook();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 4:
-                            book.showAllBooks();
-                            book.deleteBook();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 5:
-                            book.showAllBooks();
-                            book.updateBook();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 6:
-                            System.out.println("6.Powrót do glównego menu");
-                            break;
-                        default:
-                            System.out.println("Wprowadziłeś błędny numer ");
-                    }
+                Scanner scanner = new Scanner(System.in);
+                menu = scanner.nextInt();
 
-                    break;
-                case 2:
-                    System.out.println("\n1.Wyświetl wszystkich klienótw \n2.Wyszukaj klienta \n3.Dodaj klienta \n4.Usuń klienta \n5.Edytuj klienta \n6.Powrót do glównego menu");
+                switch (menu) {
+                    case 1:
+                        System.out.println("1.Wyświetl wszystkie książki \n2.Wyszukaj ksiażkę \n3.Dodaj książkę \n4.Usuń książkę \n5.Edytuj książke \n6.Powrót do glównego menu \n********************************************************");
 
-                    int menu3 = scanner.nextInt();
-                    switch (menu3) {
-                        case 1:
-                            client.showAllClients();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 2:
-                            client.showClientByNameOrSurname();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 3:
-                            client.addClient();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 4:
-                            client.deleteClient();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 5:
-                            client.updateClient();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 6:
-                            System.out.println("6.Powrót do glównego menu");
-                            break;
-                        default:
-                            System.out.println("Wprowadziłeś błędny numer ");
-                            break;
-                    }
-                case 3:
-                    System.out.println("1.Wyświetl wszystkie zamówienia \n2.Wyszukaj zamówienie \n3.Dodaj zamówienie \n4.Usuń zamówienie \n5.Edytuj zamówienie \n6.Powrót do glównego menu");
+                        int menu2 = scanner.nextInt();
+                        switch (menu2) {
+                            case 1:
+                                book.showAllBooks();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 2:
+                                book.showBooksByTitleOrAuthor();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 3:
+                                book.addBook();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 4:
+                                book.showAllBooks();
+                                book.deleteBook();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 5:
+                                book.showAllBooks();
+                                book.updateBook();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 6:
+                                System.out.println("6.Powrót do glównego menu");
+                                break;
+                            default:
+                                System.out.println("Wprowadziłeś błędny numer ");
+                        }
 
-                    int menu4 = scanner.nextInt();
-                    switch (menu4) {
-                        case 1:
-                            order.showAllOrder();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 2:
-                            order.showOrderById();
-                            jdbConnection.closeConnection();
-                            break;
-                        case 3:
-                            client.showAllClients();
-                            book.showAllBooks();
-                            book.deleteAmount(order.addOrder());
-                            jdbConnection.closeConnection();
+                        break;
+                    case 2:
+                        System.out.println("\n1.Wyświetl wszystkich klienótw \n2.Wyszukaj klienta \n3.Dodaj klienta \n4.Usuń klienta \n5.Edytuj klienta \n6.Powrót do glównego menu");
 
-                            break;
-                        case 4:
-                            order.showAllOrder();
-                            order.deleteOrder();
-                            jdbConnection.closeConnection();
+                        int menu3 = scanner.nextInt();
+                        switch (menu3) {
+                            case 1:
+                                client.showAllClients();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 2:
+                                client.showClientByNameOrSurname();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 3:
+                                client.addClient();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 4:
+                                client.deleteClient();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 5:
+                                client.updateClient();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 6:
+                                System.out.println("6.Powrót do glównego menu");
+                                break;
+                            default:
+                                System.out.println("Wprowadziłeś błędny numer ");
+                                break;
+                        }
+                    case 3:
+                        System.out.println("1.Wyświetl wszystkie zamówienia \n2.Wyszukaj zamówienie \n3.Dodaj zamówienie \n4.Usuń zamówienie \n5.Edytuj zamówienie \n6.Powrót do glównego menu");
 
-                            break;
-                        case 5:
-                            order.showAllOrder();
-                            order.updateOrder();
-                            jdbConnection.closeConnection();
+                        int menu4 = scanner.nextInt();
+                        switch (menu4) {
+                            case 1:
+                                order.showAllOrder();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 2:
+                                order.showOrderById();
+                                jdbConnection.closeConnection();
+                                break;
+                            case 3:
+                                client.showAllClients();
+                                book.showAllBooks();
+                                book.deleteAmount(order.addOrder());
+                                jdbConnection.closeConnection();
 
-                            break;
-                        case 6:
-                            System.out.println("6.Powrót do glównego menu");
-                            break;
-                        default:
-                            System.out.println("Wprowadziłeś błędny numer ");
-                    }
+                                break;
+                            case 4:
+                                order.showAllOrder();
+                                order.deleteOrder();
+                                jdbConnection.closeConnection();
 
-                    break;
-                case 0:
-                    System.out.println("Dziękuję za odwiedziny");
-                    break;
-                default:
-                    System.out.println("Wprowadziłeś blędny numer");
-            }
-        } while (menu != 0);
+                                break;
+                            case 5:
+                                order.showAllOrder();
+                                order.updateOrder();
+                                jdbConnection.closeConnection();
+
+                                break;
+                            case 6:
+                                System.out.println("6.Powrót do glównego menu");
+                                break;
+                            default:
+                                System.out.println("Wprowadziłeś błędny numer ");
+                        }
+
+                        break;
+                    case 0:
+                        System.out.println("Dziękuję za odwiedziny");
+                        break;
+                    default:
+                        System.out.println("Wprowadziłeś blędny numer");
+                }
+            } while (menu != 0);
+        }else {
+            System.out.println("Błędny login lub hasło");
+        }
 
     }
 
