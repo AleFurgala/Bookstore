3bcbfc78056f99d5b16789327de38e15b1976272
AleFurgala <ale.furgala@gmail.com>
2023-07-20 17:37:03 +0200

    added OrderTest.java

---
+++ src/test/OrderTest.java
@@ -0,0 +1,15 @@
+package test;
+
+
+import main.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void getDate() {
+
+    }
+}
