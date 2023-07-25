c310b3e6d39233b7564948acdcc8c3a4191cdf33
AleFurgala <ale.furgala@gmail.com>
2023-07-25 20:45:07 +0200

    showAllClientsByNameOrSurname test was added

--- src/test/ClientTest.java
+++ src/test/ClientTest.java
@@ -54,7 +54,40 @@ class ClientTest {
     }
 
     @Test
-    void showClientByNameOrSurname() {
+    void showClientByNameOrSurname() throws SQLException {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(url, user, password);
+                System.out.println("Połączono z bazą danych");
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            try {
+
+                Statement stmt = connection.createStatement();
+                String query = "CREATE TABLE test_klienci (id INT AUTO_INCREMENT, imie VARCHAR(255), nazwisko VARCHAR(255), adres VARCHAR(255), PRIMARY KEY (id))";
+                stmt.execute(query);
+
+                String query2 = "INSERT INTO test_klienci (imie, nazwisko, adres) VALUES('Jan' ,'Nowak', 'Rzeszow'), ('Krystyna' , 'Kowalska' , 'Warszawa')";
+                stmt.execute(query2);
+
+                String query3 = "SELECT * FROM test_klienci WHERE imie = 'Jan' OR nazwisko = 'Nowak'";
+                ResultSet rs = stmt.executeQuery(query3);
+                String output = "";
+                while (rs.next()) {
+                    output = rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4);
+                }
+
+                String expectedOutput = "1  Jan  Nowak  Rzeszow";
+                assertEquals(expectedOutput, output);
+
+                String query4 = "DROP table test_klienci";
+                stmt.execute(query4);
+                connection.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
     }
 
     @Test
