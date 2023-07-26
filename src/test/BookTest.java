5142cbdca63f291480418376788acd46754ae62d
AleFurgala <ale.furgala@gmail.com>
2023-07-26 17:57:20 +0200

    BookTest.java was added

---
+++ src/test/BookTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+public class BookTest {
+    private static final String url = "jdbc:mysql://localhost:3306/ksiegarnia";
+    private static final String user = "root";
+    private static final String password = "";
+
+    private Connection connection;
+
+    @Test
+    void showAllBooks() {
+    }
+
+    @Test
+    void showBooksByTitleOrAuthor() {
+    }
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void deleteAmount() {
+    }
+}
