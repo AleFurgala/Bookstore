4a13acc2c9c88fa01b0c3f53cab986166be71355
AleFurgala <ale.furgala@gmail.com>
2023-07-27 17:57:14 +0200

    getPasswordBasedLogin Test was added

--- src/test/AdminAccountsTest.java
+++ src/test/AdminAccountsTest.java
@@ -2,7 +2,9 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AdminAccountsTest {
     private static final String url = "jdbc:mysql://localhost:3306/ksiegarnia";
@@ -12,7 +14,43 @@ public class AdminAccountsTest {
     private Connection connection;
 
     @Test
-    void getPasswordBasedLogin() {
+    void getPasswordBasedLogin() throws SQLException {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Połączono z bazą danych");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        try {
+
+            Statement stmt = connection.createStatement();
+            String query = "CREATE TABLE test_konta_administratorow (id INT AUTO_INCREMENT, login VARCHAR(255), haslo VARCHAR(255), nazwa_uzytkownika VARCHAR(255), PRIMARY KEY (id))";
+            stmt.execute(query);
+
+            String query2 = "INSERT INTO test_konta_administratorow (login, haslo, nazwa_uzytkownika) VALUES('admin','ddd','Jurek'), ('admin2' ,'aaa','Krysia')";
+            stmt.execute(query2);
+
+            String query3 = "SELECT haslo FROM test_konta_administratorow WHERE login ='admin'";
+            ResultSet rs = stmt.executeQuery(query3);
+            String output = "";
+            while (rs.next()) {
+                output =  rs.getString(1);
+            }
+
+            String expectedOutput = "ddd";
+            assertEquals(expectedOutput, output);
+
+            String query4 = "DROP table test_konta_administratorow";
+            stmt.execute(query4);
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
     }
 
     @Test
