0635cd0a3a5f53c173e7301e5518005573d09698
AleFurgala <ale.furgala@gmail.com>
2023-07-27 15:04:15 +0200

    AdminAccountsTest.java was added

---
+++ src/test/AdminAccountsTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+public class AdminAccountsTest {
+    private static final String url = "jdbc:mysql://localhost:3306/ksiegarnia";
+    private static final String user = "root";
+    private static final String password = "";
+
+    private Connection connection;
+
+    @Test
+    void getPasswordBasedLogin() {
+    }
+
+    @Test
+    void getNameBasedLogin() {
+    }
+
+    @Test
+    void addAdminAccount() {
+    }
+
+    @Test
+    void deleteAdminAccount() {
+    }
+
+    @Test
+    void dataEncryption() {
+    }
+
+    @Test
+    void dataDecryption() {
+    }
+}
