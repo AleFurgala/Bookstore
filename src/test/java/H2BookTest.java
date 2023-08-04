c58ccc0c67f4c2af5c38853d2ea5142355310dee
AleFurgala <ale.furgala@gmail.com>
2023-08-04 12:52:34 +0200

    showAllBooksTest was updated

--- src/test/java/H2BookTest.java
+++ src/test/java/H2BookTest.java
@@ -36,30 +36,26 @@ public class H2BookTest {
 
         Statement statement = connection.createStatement();
 
-        // Utworzenie tabeli "users"
         statement.executeUpdate("CREATE TABLE ksiazki (id INT AUTO_INCREMENT, tytul VARCHAR(255), autor VARCHAR(255), cena INT, ilosc INT, PRIMARY KEY (id))");
 
-        // Wstawienie danych testowych
         statement.executeUpdate("INSERT INTO ksiazki (tytul, autor, cena, ilosc) VALUES('Water' , 'Paula Hawkins' , 23 , 2)");
         statement.executeUpdate("INSERT INTO ksiazki (tytul, autor, cena, ilosc) VALUES('xyz' , 'abc' , 30 , 3)");
 
-        statement.close();
         Book book = new Book(connection);
         book.showAllBooks();
 
-
-        // Assert
-        // Dla testów jednostkowych możemy skupić się na przechwyceniu wyników i ich analizie
-        // zamiast sprawdzania wydruków na konsoli. W tym przykładzie użyjemy ResultSetSpy, który pozwala
-        // na przechwycenie wyników zapytania i ich analizę.
-
-        ResultSetSpy resultSetSpy = new ResultSetSpy(connection.createStatement().executeQuery("SELECT * FROM ksiazki"));
+        //ResultSetSpy resultSetSpy = new ResultSetSpy(connection.createStatement().executeQuery("SELECT * FROM ksiazki"));
         //assertEquals(2, resultSetSpy.getCount()); // Oczekujemy 2 rekordów
-        assertEquals("Water", resultSetSpy.getRow(1).getString("tytul"));
-        assertEquals("Paula Hawkins", resultSetSpy.getRow(1).getString("autor"));
-        assertEquals("xyz", resultSetSpy.getRow(2).getString("tytul"));
-        assertEquals("abc", resultSetSpy.getRow(2).getString("autor"));
+        //assertEquals("Water", resultSetSpy.getRow(1).getString("tytul"));
 
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM ksiazki");
+        resultSet.absolute(1);
+        assertEquals("Water", resultSet.getString(2));
+        assertEquals("Paula Hawkins", resultSet.getString(3));
+        resultSet.absolute(2);
+        assertEquals("xyz", resultSet.getString(2));
+        assertEquals("abc", resultSet.getString(3));
+        statement.close();
     }
 
     @Test
