b4ca5d93b0ae0056460aba890b2ea54867aec988
AleFurgala <ale.furgala@gmail.com>
2023-08-11 18:54:12 +0200

    useless tests was deleted

---
+++ src/test/java/com/example/OrderTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OrderTest {
+    @Test
+    void getDate() {
+        Order order = new Order();
+
+        String result = order.getDate();
+        Calendar calendar = Calendar.getInstance();
+        int expectedYear = calendar.get(Calendar.YEAR);
+        int expectedMonth = calendar.get(Calendar.MONTH) + 1;
+        int expectedDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        String expectedDate = expectedDayOfMonth + "" + expectedMonth + "" + expectedYear;
+        assertEquals(expectedDate, result);
+    }
+}
