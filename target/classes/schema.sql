67c56691164a97bac868f0cc443dc543f3382a7d
AleFurgala <ale.furgala@gmail.com>
2023-08-25 19:25:40 +0200

    Liquibase updated

---
+++ target/classes/schema.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+--changeset AleFurgala:1
+
+CREATE TABLE klienci (id INT AUTO_INCREMENT, imie VARCHAR(255), nazwisko VARCHAR(255), adres VARCHAR(255), PRIMARY KEY (id));
+CREATE TABLE konta_administratorow (id INT AUTO_INCREMENT, login VARCHAR(255), haslo VARCHAR(255), nazwa_uzytkownika VARCHAR(255), PRIMARY KEY (id));
+CREATE TABLE ksiazki (id INT AUTO_INCREMENT, tytul VARCHAR(255), autor VARCHAR(255), cena INT, ilosc INT, PRIMARY KEY (id));
+CREATE TABLE zamowienia (id INT AUTO_INCREMENT, id_klienci INT, id_ksiazki INT, data VARCHAR(255), PRIMARY KEY (id), FOREIGN KEY (id_klienci) REFERENCES klienci(id), FOREIGN KEY (id_ksiazki) REFERENCES ksiazki(id));
+
+
