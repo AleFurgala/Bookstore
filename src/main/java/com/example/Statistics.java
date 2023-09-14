4f169e7d1dec973716e440716e0781cec1aef7b3
AleFurgala <ale.furgala@gmail.com>
2023-09-14 16:26:32 +0200

    Statistics.java was updated

--- src/main/java/com/example/Statistics.java
+++ src/main/java/com/example/Statistics.java
@@ -10,21 +10,22 @@ public class Statistics {
 
 
     public void showOrderStatistic() throws SQLException {
-        String query = "SELECT id_ksiazki, COUNT(*) AS liczba_zamowionych_ksiazek FROM zamowienia GROUP BY id_ksiazki ORDER BY liczba_zamowionych_ksiazek DESC";
+        String query = "SELECT id_ksiazki, ksiazki.tytul, COUNT(*) AS liczba_zamowionych_ksiazek FROM zamowienia INNER JOIN ksiazki ON ksiazki.id = zamowienia.id_ksiazki GROUP BY id_ksiazki ORDER BY liczba_zamowionych_ksiazek DESC";
 
         try {
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
-            System.out.println(" id książki       | liczba zamówionych książek ");
-            System.out.println("---------------------------------------------------------------");
+            System.out.println(" id książki       |Tytuł                            | liczba zamówionych książek ");
+            System.out.println("--------------------------------------------------------------------------------------------");
             System.out.println();
             while (rs.next()) {
                 Long column1 = rs.getLong(1);
-                Long column2 = rs.getLong(2);
+                String column2 = rs.getString(2);
+                Long column3 = rs.getLong(3);
 
-                System.out.printf("%-20d %-20s%n", column1, column2);
+                System.out.printf("%-20d %-20s  %-20s%n", column1,column2, column3);
             }
-            System.out.println("---------------------------------------------------------------");
+            System.out.println("--------------------------------------------------------------------------------------------");
         } catch (Exception e) {
             System.out.println(e);
         }
